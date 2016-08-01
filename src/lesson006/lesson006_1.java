fe667af97ca7f86298644b56c2ae8ca71d5014b6
comboalx <comboalx@gmail.com>
2016-08-02 00:11:28 +0300

    Добавление 6-й

---
+++ src/lesson006/lesson006_1.java
@@ -0,0 +1,14 @@
+package lesson006;
+
+/**
+ * Created by Owner on 01.08.2016.
+ */
+public class lesson006_1 {
+    public static void main (String [] args) {
+        int a=1, b=2, c=3;
+        System.out.println(a+=5);
+        System.out.println(b*=4);
+        System.out.println(c+=a*b);
+        System.out.println(c%=6);
+    }
+}
