9de57d8fa2d2c666ecfa7ea3ec8fa22f1c10b30f
comboalx <comboalx@gmail.com>
2016-08-02 22:29:13 +0300

    Добавление 7-й

---
+++ src/lesson007/lesson007.java
@@ -0,0 +1,22 @@
+package lesson007;
+
+/**
+ * Created by Owner on 02.08.2016.
+ */
+public class lesson007 {
+    public static void main (String [] args) {
+        int month = 4;
+        String season;
+        if (month == 12 || month == 1 || month == 2)
+            season = "winter";
+        else if (month == 3 || month == 4 || month == 5)
+            season = "spring";
+        else if (month == 6 || month == 7 || month == 8)
+            season = "summer";
+        else if (month == 9 || month == 10 || month == 11)
+            season = "fol";
+        else
+            season = "it's a dream";
+        System.out.println("April is - " + season);
+    }
+}
